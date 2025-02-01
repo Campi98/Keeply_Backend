b7d2102a9b563a3fa74f78eba61508b6b08c1df0
Campi98 <121031836+Campi98@users.noreply.github.com>
2025-02-01 19:25:49 +0000

    fix manhoso ao delete

--- src/main/kotlin/com/example/demo/repository/NoteRepository.kt
+++ src/main/kotlin/com/example/demo/repository/NoteRepository.kt
@@ -1,10 +1,28 @@
 package com.example.demo.repository
 
+import jakarta.persistence.EntityManager
 import com.example.demo.model.Note
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+
+interface CustomNoteRepository {
+    fun deleteByUserId(userId: Int)
+}
 
 @Repository
-interface NoteRepository : JpaRepository<Note, Long>{
-    fun deleteByUserId(userId: Int)
+class CustomNoteRepositoryImpl(
+    private val entityManager: EntityManager
+) : CustomNoteRepository {
+    
+    @Transactional
+    override fun deleteByUserId(userId: Int) {
+        val query = entityManager.createQuery("DELETE FROM Note n WHERE n.userId = :userId")
+        query.setParameter("userId", userId)
+        query.executeUpdate()
+    }
+}
+
+@Repository
+interface NoteRepository : JpaRepository<Note, Long>, CustomNoteRepository {
 }
